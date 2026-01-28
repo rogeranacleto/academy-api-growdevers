f0d376636d148b7578459dc8a07fc0b68060f53d
Roger Anacleto <rogerdasilvacode7@gmail.com>
2026-01-27 23:40:20 -0300

    feat: user base

---
+++ src/dados.js
@@ -0,0 +1,38 @@
+import { randomUUID } from 'crypto'
+export const growdevers = [
+  {
+    id: randomUUID(),
+    nome: "Roger",
+    email: "roger@roger.com",
+    idade: 23,
+    matriculado: true,
+  },
+  {
+    id: randomUUID(),
+    nome: "Clara",
+    email: "clara@clara.com",
+    idade: 23,
+    matriculado: true,
+  },
+  {
+    id: randomUUID(),
+    nome: "Luiza",
+    email: "luiza@luiza.com",
+    idade: 23,
+    matriculado: true,
+  },
+  {
+    id: randomUUID(),
+    nome: "Daphne",
+    email: "daphne@daphne.com",
+    idade: 25,
+    matriculado: true,
+  },
+  {
+    id: randomUUID(),
+    nome: "Sofia",
+    email: "sofia@sofia.com",
+    idade: 21,
+    matriculado: true,
+  },
+];
